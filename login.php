d9fd0a30c4cc619be4302d8a37d53960eb145236
T0bmast4 <99271449+T0bmast4@users.noreply.github.com>
2023-05-17 12:34:15 +0200

    database connection

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
 <?php
+$dsn = "mysql:host=localhost;dbname=accounts";
+$mysql_username = "root";
+$mysql_password = "";
 
+try {
+    $db = new PDO($dsn, $mysql_username, $mysql_password);
+} catch (PDOException $e) {
+    
+}
 ?>
