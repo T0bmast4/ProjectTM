675963adce6a55f173b4da2dea59015fc4ce12bc
T0bmast4 <99271449+T0bmast4@users.noreply.github.com>
2023-05-17 12:56:01 +0200

    test

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=accounts";
-$mysql_username = "root";
-$mysql_password = "";
+$dsn = "mysql:host=38.242.141.75;dbname=accounts";
+
+$mysql_username = "USERNAME";
+$mysql_password = "PASSWORD";
 
 try {
     $db = new PDO($dsn, $mysql_username, $mysql_password);
